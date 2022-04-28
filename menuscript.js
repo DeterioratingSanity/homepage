5b2135ae4d23c16d4d8b7ba9f3492bf55a41c775
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-04-28 08:24:02 -0400

    Update menuscript.js

--- menuscript.js
+++ menuscript.js
@@ -21,3 +21,17 @@ function closeNav() {
   up = 0;
   document.getElementById("key").style.transform = "rotateY(0deg)";
 }
+
+//proximity
+
+var proximitySwitch = event.screenX;
+
+let screenLog = document.querySelector('#screen-log');
+document.addEventListener('mousemove', logKey);
+
+function logKey(e) {
+  screenLog.innerText = `
+    Screen X/Y: ${e.screenX}, ${e.screenY}
+    Client X/Y: ${e.clientX}, ${e.clientY}`;
+}
+
