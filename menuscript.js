0d4117e34cbf60bde12537fc06f86b08d52348e7
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-04-27 20:17:13 -0400

    Create menuscript.js

---
+++ menuscript.js
@@ -0,0 +1,23 @@
+var up = 0;
+    
+function assess() {
+  if (up === 0) {
+    openNav();
+  } else if (up === 1) {
+    closeNav(); 
+  } 
+}
+    
+function openNav() {
+  document.getElementById("sideNavBox").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  up = 1;
+  document.getElementById("key").style.transform = "rotateY(180deg)";
+}
+        
+function closeNav() {
+  document.getElementById("sideNavBox").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  up = 0;
+  document.getElementById("key").style.transform = "rotateY(0deg)";
+}
