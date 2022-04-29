aa8229b07d055a2c86c01f78d4cbcbc139e7287a
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-04-28 20:19:20 -0400

    Update menuscript.js

--- menuscript.js
+++ menuscript.js
@@ -35,16 +35,29 @@ function proximitySwitch(e){
   }
 }
 
+//touchscreen
 
-/**
-var proximityX = pageX;
+var touch = document.querySelector("#main");
+var threshhold = 100;
+let initialX = 0;
+let moveX = 0;
 
-document.onclick = function(proximity) {
-  proximityX = event.clientX;
+touch.addEventListener("touchstart", e => {
+   initialX = e.touches[0].clientX;   
+;
+
+touch.addEventListener("touchmove", e => {
+   let currentX = e.touches[0].clientX;    
+   moveX = currentX - initialX;
+); 
+
+touch.addEventListener("touchend", e => {
+   if (moveX > threshold * Math.sign(moveX) && up === 0) {
+      openNav();
+   } else if (moveX < threshhold * Math.sign(moveX) && up === 1) {
+      closeNav();   
+   }
 }
-if (proximityX < 100) {
-  assess();
-  document.getElementById("gremlin").innerHTML = "proximityX"; 
-}
-**/
-
+                       
+                       
+                       
