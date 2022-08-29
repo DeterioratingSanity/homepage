f6c20a282485e26605abf53efe6f483c27890595
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-08-28 22:24:38 -0400

    scroll
    fancier js scroll indicator

--- menuscript.js
+++ menuscript.js
@@ -104,6 +104,15 @@ function headerSwitch() {
     document.getElementById("titleBox").style.boxShadow = "0 0 #473B38";
   }
 }
+
+window.onscroll = function() {scrollBar()};
+
+function scrollBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("bar").style.width = scrolled + "%";
+}
 /*
 document.addEventListener('scroll',scrollBar);
 
