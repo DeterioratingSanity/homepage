05bb89947722bf52e1ce93abdfc8979bde32ae51
Wyatt Wilson <95758153+DeterioratingSanity@users.noreply.github.com>
2022-09-11 19:56:55 -0400

    modal for the butotn
    functionality is yeeeepe

---
+++ modal.js
@@ -0,0 +1,20 @@
+//modal
+var modal = document.getElementById("modalBox");
+
+var btn = document.getElementById("modalButton");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
